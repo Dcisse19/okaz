280d90739d3f7566046b2b2ded7fbe99303f2fd1
Diambere <diambere.c@gmail.com>
2022-08-30 12:19:12 +0200

    first commit

---
+++ includes/header.php
@@ -0,0 +1,61 @@
+<div class="">
+        <header class="py-3 px-20 bg-slate-900 ">
+            <div class="container flex mx-auto justify-between items-center">
+                <div class="flex place-items-center gap-3">
+                    <i class="fa-brands fa-facebook-f fa-lg" style="--fa-primary-color: white"></i>
+                    <i class="fa-brands fa-instagram fa-lg"></i>
+                    <i class="fa-brands fa-twitter fa-lg"></i>
+                </div>
+                <div class="hidden justify-between items-center gap-5">
+                    <div class=" bg-amber-500 rounded-lg flex gap-3 place-items-center">
+                        <i class="pl-2 fa-solid fa-circle-user"></i>
+                        <p class="pr-4 text-white">Mon compte</p>
+                    </div>
+                    <i class="place-self-center fa-solid fa-cart-arrow-down fa-lg"></i>
+                </div>
+                <div class="flex justify-between items-center gap-5">
+                    <ul class="list-none flex gap-6 justify-between items-center">
+                        <li>
+                            <button id="loginLink" class="text-lg font-light text-white hover:text-amber-500">
+                                <!-- <a class="text-lg font-light text-white hover:text-amber-500" href="#"> -->
+                                <i class="fa-solid fa-arrow-right-to-bracket mr-3"></i>Connexion
+                                <!-- </a> -->
+                            </button>
+                        </li>
+                        <li>
+                            <a id="signupLink" class="text-lg font-light text-white hover:text-amber-500" href="#" data-bs-toggle="modal">
+                                <i class="fa-solid fa-inbox mr-3"></i>Inscription</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+        <nav class="py-4 px-20 border-b-slate-400 border-2 bg-white ">
+            <div class="container mx-auto flex justify-between items-center">
+                <a href="index.php">
+                    <img class="w-20" src="./images/logo_okaz.png" alt="logo_okaz">
+                </a>
+                <ul class="list-none flex gap-6 justify-between items-center">
+                    <li>
+                        <a class="text-lg text-gray-700 hover:text-amber-500 active:text-amber-500" href="index.php">Acceuil</a>
+                    </li>
+                    <li>
+                        <a class="text-lg text-gray-700 hover:text-amber-500 active:text-amber-500" href="#">A propos</a>
+                    </li>
+                    <li>
+                        <a class="text-lg text-gray-700 hover:text-amber-500 active:text-amber-500" href="#">Boutique</a>
+                    </li>
+                    <li>
+                        <a class="text-lg text-gray-700 hover:text-amber-500 active:text-amber-500" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <section class="bg-gray-300 shadow-slate-400 shadow-md py-24 px-40">
+            <div class="container mx-auto flex-col items-center">
+                <h1 class="text-5xl text-center font-light uppercase mb-10">Titre</h1>
+                <p class="text-center font-light">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ab modi earum perferendis eos nulla eaque, illum officia cupiditate asperiores velit ullam at veniam. Dicta dolorem quo mollitia sequi rerum perspiciatis corrupti iusto esse expedita. Provident expedita aperiam sed numquam explicabo. Iure, aspernatur quidem iusto totam quis perferendis modi error.</p>
+            </div>
+        </section>
+    </div>
